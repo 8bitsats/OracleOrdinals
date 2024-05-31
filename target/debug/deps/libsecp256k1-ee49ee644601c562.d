d967ffe57fc4d02d2257c2d38307f4a9141ecfc3
8 bit <8bit@ordlibrary.com>
2024-05-31 16:55:22 -0400

    fff

---
+++ target/debug/deps/libsecp256k1-ee49ee644601c562.d
@@ -0,0 +1,9 @@
+/Users/ordlibrary/OracleOrdinals/target/debug/deps/liblibsecp256k1-ee49ee644601c562.rmeta: /Users/ordlibrary/.cargo/registry/src/index.crates.io-6f17d22bba15001f/libsecp256k1-0.6.0/src/lib.rs /Users/ordlibrary/OracleOrdinals/target/debug/build/libsecp256k1-a499074e0e51d083/out/const.rs /Users/ordlibrary/OracleOrdinals/target/debug/build/libsecp256k1-a499074e0e51d083/out/const_gen.rs
+
+/Users/ordlibrary/OracleOrdinals/target/debug/deps/libsecp256k1-ee49ee644601c562.d: /Users/ordlibrary/.cargo/registry/src/index.crates.io-6f17d22bba15001f/libsecp256k1-0.6.0/src/lib.rs /Users/ordlibrary/OracleOrdinals/target/debug/build/libsecp256k1-a499074e0e51d083/out/const.rs /Users/ordlibrary/OracleOrdinals/target/debug/build/libsecp256k1-a499074e0e51d083/out/const_gen.rs
+
+/Users/ordlibrary/.cargo/registry/src/index.crates.io-6f17d22bba15001f/libsecp256k1-0.6.0/src/lib.rs:
+/Users/ordlibrary/OracleOrdinals/target/debug/build/libsecp256k1-a499074e0e51d083/out/const.rs:
+/Users/ordlibrary/OracleOrdinals/target/debug/build/libsecp256k1-a499074e0e51d083/out/const_gen.rs:
+
+# env-dep:OUT_DIR=/Users/ordlibrary/OracleOrdinals/target/debug/build/libsecp256k1-a499074e0e51d083/out
