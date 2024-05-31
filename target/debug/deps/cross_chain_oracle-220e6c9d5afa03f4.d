5d32e342285462020bf71ff3ce00492158f18d0d
8 bit <8bit@ordlibrary.com>
2024-05-31 16:06:52 -0400

    ff

---
+++ target/debug/deps/cross_chain_oracle-220e6c9d5afa03f4.d
@@ -0,0 +1,5 @@
+/Users/ordlibrary/OracleOrdinals/target/debug/deps/libcross_chain_oracle-220e6c9d5afa03f4.rmeta: programs/cross_chain_oracle/src/lib.rs
+
+/Users/ordlibrary/OracleOrdinals/target/debug/deps/cross_chain_oracle-220e6c9d5afa03f4.d: programs/cross_chain_oracle/src/lib.rs
+
+programs/cross_chain_oracle/src/lib.rs:
---
+++ target/debug/deps/cross_chain_oracle-850b4bf5ac7ff397.d
@@ -0,0 +1,5 @@
+/Users/ordlibrary/OracleOrdinals/target/debug/deps/libcross_chain_oracle-850b4bf5ac7ff397.rmeta: programs/cross_chain_oracle/src/lib.rs
+
+/Users/ordlibrary/OracleOrdinals/target/debug/deps/cross_chain_oracle-850b4bf5ac7ff397.d: programs/cross_chain_oracle/src/lib.rs
+
+programs/cross_chain_oracle/src/lib.rs:
