20e2c14ec2a0f840022d6909847d282ff9cf1515
8 bit <8bit@ordlibrary.com>
2024-05-31 16:53:57 -0400

    f

---
+++ target/debug/deps/syn_derive-b8889293278823a9.d
@@ -0,0 +1,5 @@
+/Users/ordlibrary/OracleOrdinals/target/debug/deps/libsyn_derive-b8889293278823a9.dylib: /Users/ordlibrary/.cargo/registry/src/index.crates.io-6f17d22bba15001f/syn_derive-0.1.8/src/lib.rs
+
+/Users/ordlibrary/OracleOrdinals/target/debug/deps/syn_derive-b8889293278823a9.d: /Users/ordlibrary/.cargo/registry/src/index.crates.io-6f17d22bba15001f/syn_derive-0.1.8/src/lib.rs
+
+/Users/ordlibrary/.cargo/registry/src/index.crates.io-6f17d22bba15001f/syn_derive-0.1.8/src/lib.rs:
---
+++ target/debug/deps/zeroize_derive-f8866c60210222ff.d
@@ -0,0 +1,5 @@
+/Users/ordlibrary/OracleOrdinals/target/debug/deps/libzeroize_derive-f8866c60210222ff.dylib: /Users/ordlibrary/.cargo/registry/src/index.crates.io-6f17d22bba15001f/zeroize_derive-1.4.2/src/lib.rs
+
+/Users/ordlibrary/OracleOrdinals/target/debug/deps/zeroize_derive-f8866c60210222ff.d: /Users/ordlibrary/.cargo/registry/src/index.crates.io-6f17d22bba15001f/zeroize_derive-1.4.2/src/lib.rs
+
+/Users/ordlibrary/.cargo/registry/src/index.crates.io-6f17d22bba15001f/zeroize_derive-1.4.2/src/lib.rs:
