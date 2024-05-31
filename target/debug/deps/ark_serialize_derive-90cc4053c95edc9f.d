2e861fe25f2298e5c72d41d2fc3568f4b470ed76
8 bit <8bit@ordlibrary.com>
2024-05-31 16:53:52 -0400

    ff

---
+++ target/debug/deps/ark_serialize_derive-90cc4053c95edc9f.d
@@ -0,0 +1,7 @@
+/Users/ordlibrary/OracleOrdinals/target/debug/deps/libark_serialize_derive-90cc4053c95edc9f.dylib: /Users/ordlibrary/.cargo/registry/src/index.crates.io-6f17d22bba15001f/ark-serialize-derive-0.4.2/src/lib.rs /Users/ordlibrary/.cargo/registry/src/index.crates.io-6f17d22bba15001f/ark-serialize-derive-0.4.2/src/serialize.rs /Users/ordlibrary/.cargo/registry/src/index.crates.io-6f17d22bba15001f/ark-serialize-derive-0.4.2/src/deserialize.rs
+
+/Users/ordlibrary/OracleOrdinals/target/debug/deps/ark_serialize_derive-90cc4053c95edc9f.d: /Users/ordlibrary/.cargo/registry/src/index.crates.io-6f17d22bba15001f/ark-serialize-derive-0.4.2/src/lib.rs /Users/ordlibrary/.cargo/registry/src/index.crates.io-6f17d22bba15001f/ark-serialize-derive-0.4.2/src/serialize.rs /Users/ordlibrary/.cargo/registry/src/index.crates.io-6f17d22bba15001f/ark-serialize-derive-0.4.2/src/deserialize.rs
+
+/Users/ordlibrary/.cargo/registry/src/index.crates.io-6f17d22bba15001f/ark-serialize-derive-0.4.2/src/lib.rs:
+/Users/ordlibrary/.cargo/registry/src/index.crates.io-6f17d22bba15001f/ark-serialize-derive-0.4.2/src/serialize.rs:
+/Users/ordlibrary/.cargo/registry/src/index.crates.io-6f17d22bba15001f/ark-serialize-derive-0.4.2/src/deserialize.rs:
